09bf8c489251c4401d06cba427c39f3290459c07
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "storage_queues" {
-  description = "All storage_queue resources"
-  value       = azurerm_storage_queue.storage_queues
-}
 output "storage_queues_metadata" {
-  description = "List of metadata values across all storage_queues"
-  value       = [for k, v in azurerm_storage_queue.storage_queues : v.metadata]
+  description = "Map of metadata values across all storage_queues, keyed the same as var.storage_queues"
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.metadata }
 }
 output "storage_queues_name" {
-  description = "List of name values across all storage_queues"
-  value       = [for k, v in azurerm_storage_queue.storage_queues : v.name]
+  description = "Map of name values across all storage_queues, keyed the same as var.storage_queues"
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.name }
 }
 output "storage_queues_resource_manager_id" {
-  description = "List of resource_manager_id values across all storage_queues"
-  value       = [for k, v in azurerm_storage_queue.storage_queues : v.resource_manager_id]
+  description = "Map of resource_manager_id values across all storage_queues, keyed the same as var.storage_queues"
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.resource_manager_id }
 }
 output "storage_queues_storage_account_id" {
-  description = "List of storage_account_id values across all storage_queues"
-  value       = [for k, v in azurerm_storage_queue.storage_queues : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_queues, keyed the same as var.storage_queues"
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.storage_account_id }
 }
 output "storage_queues_storage_account_name" {
-  description = "List of storage_account_name values across all storage_queues"
-  value       = [for k, v in azurerm_storage_queue.storage_queues : v.storage_account_name]
+  description = "Map of storage_account_name values across all storage_queues, keyed the same as var.storage_queues"
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.storage_account_name }
 }
 output "storage_queues_url" {
-  description = "List of url values across all storage_queues"
-  value       = [for k, v in azurerm_storage_queue.storage_queues : v.url]
+  description = "Map of url values across all storage_queues, keyed the same as var.storage_queues"
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.url }
 }
 
