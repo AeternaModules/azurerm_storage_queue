48ad5c0ae3b7e221de2e22ad955c601e069b1035
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "storage_queues_id" {
   description = "Map of id values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.id }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_queues_metadata" {
   description = "Map of metadata values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.metadata }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.metadata if v.metadata != null && length(v.metadata) > 0 }
 }
 output "storage_queues_name" {
   description = "Map of name values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.name }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "storage_queues_resource_manager_id" {
   description = "Map of resource_manager_id values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.resource_manager_id }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.resource_manager_id if v.resource_manager_id != null && length(v.resource_manager_id) > 0 }
 }
 output "storage_queues_storage_account_id" {
   description = "Map of storage_account_id values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.storage_account_id }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.storage_account_id if v.storage_account_id != null && length(v.storage_account_id) > 0 }
 }
 output "storage_queues_storage_account_name" {
   description = "Map of storage_account_name values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.storage_account_name }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.storage_account_name if v.storage_account_name != null && length(v.storage_account_name) > 0 }
 }
 output "storage_queues_url" {
   description = "Map of url values across all storage_queues, keyed the same as var.storage_queues"
-  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.url }
+  value       = { for k, v in azurerm_storage_queue.storage_queues : k => v.url if v.url != null && length(v.url) > 0 }
 }
 
